2258744f74c6223cb18f5948cc4af0617d6b7d8d
tim ichiba <ichibat@gmail.com>
2019-09-23 10:40:34 +0900

    vue part works well

--- client/src/components/PostComponent.vue
+++ client/src/components/PostComponent.vue
@@ -1,11 +1,43 @@
 <template>
-  <h1>hello!</h1>
+  <div class="container">
+    <h1>Latest Posts</h1>
+    <!-- CREATE POST HERE -->
+    <hr />
+    <p class="error" v-if="error">{{error}}</p>
+    <div class="posts-container">
+      <div
+        class="post"
+        v-for="(post,index) in posts"
+        v-bind:item="post"
+        v-bind:index="index"
+        v-bind:key="post._id"
+      >
+        {{ `${post.createdAt.getDate()}/${post.createdAt.getMonth()}/${post.createdAt.getFullYear()}`}}
+        <p class="text">{{post.text}}</p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import PostService from "../PostService";
+
 export default {
   name: "PostComponent",
-  data() {}
+  data() {
+    return {
+      posts: [],
+      error: "",
+      text: ""
+    };
+  },
+  async created() {
+    try {
+      this.posts = await PostService.getPosts();
+    } catch (err) {
+      this.error = err.message;
+    }
+  }
 };
 </script>
 
